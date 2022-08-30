399ec6627dc7fdb309206f924aad29ee1ba19590
reinahandal <handalreina@gmail.com>
2022-08-30 16:47:10 +0300

    layout for entire page, adding like buttons to each individual status update

--- Javascript/likes/script.js
+++ Javascript/likes/script.js
@@ -1,7 +1,23 @@
-var likes = document.querySelector("#likes-1");
-var newLikes = 3;
+var likes1 = document.querySelector("#likes-1");
+var newLikes1 = 3;
 
-function increaseLikes() {
-    newLikes++;
-    likes.innerText = newLikes + " like(s)";
+function increaseLikes1() {
+    newLikes1++;
+    likes1.innerText = newLikes1 + " like(s)";
+}
+
+var likes2 = document.querySelector("#likes-2");
+var newLikes2 = 12;
+
+function increaseLikes2() {
+    newLikes2++;
+    likes2.innerText = newLikes2 + " like(s)";
+}
+
+var likes3 = document.querySelector("#likes-3");
+var newLikes3 = 9;
+
+function increaseLikes3() {
+    newLikes3++;
+    likes3.innerText = newLikes3 + " like(s)";
 }
