2bbbf06efbd37e2ee0a62d2318bf492fe5d31330
reinahandal <handalreina@gmail.com>
2022-08-30 13:18:23 +0300

    solution of main challenge

---
+++ Javascript/dojo-pizzeria/script.js
@@ -0,0 +1,19 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+var pizza1 = pizzaOven("deepdish", "traditional", "mozzarella", "[pepperoni, sausage]");
+console.log(pizza1);
+
+var pizza2 = pizzaOven("hand tossed", "marinara", "[mozzarella, feta]", "[mushrooms, olives, onions]");
+console.log(pizza2);
+
+var pizza3 = pizzaOven("stuffed crust", "basil tomato", "buffalo mozzarella", "[mushrooms, black olives, red peppers");
+console.log(pizza3);
+
+var pizza4 = pizzaOven("thin crust", "traditional", "[mozzarella, gorgonzola, parmisan, goat cheese]", "[cherry tomatoes, basil");
+console.log(pizza4);
