d4d8e912f82a60c85eaeac56c6c214240d1d43ff
reinahandal <handalreina@gmail.com>
2022-08-30 19:58:32 +0300

    different function to increment likes

--- Javascript/likes/script.js
+++ Javascript/likes/script.js
@@ -1,23 +1,19 @@
-var likes1 = document.querySelector("#likes-1");
-var newLikes1 = 3;
-
 function increaseLikes1() {
+    var likes1 = document.querySelector("#likes-1 span");
+    var newLikes1 = likes1.innerHTML;
     newLikes1++;
-    likes1.innerText = newLikes1 + " like(s)";
-}
-
-var likes2 = document.querySelector("#likes-2");
-var newLikes2 = 12;
+    likes1.innerHTML = newLikes1;
+} 
 
 function increaseLikes2() {
+    var likes2 = document.querySelector("#likes-2 span");
+    var newLikes2 = likes2.innerHTML;
     newLikes2++;
-    likes2.innerText = newLikes2 + " like(s)";
-}
-
-var likes3 = document.querySelector("#likes-3");
-var newLikes3 = 9;
-
+    likes2.innerHTML = newLikes2;
+} 
 function increaseLikes3() {
+    var likes3 = document.querySelector("#likes-3 span");
+    var newLikes3 = likes3.innerHTML;
     newLikes3++;
-    likes3.innerText = newLikes3 + " like(s)";
-}
+    likes3.innerHTML = newLikes3;
+} 
