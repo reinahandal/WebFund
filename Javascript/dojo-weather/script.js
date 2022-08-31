9268fd87bb4f8085a78e5920610ab44b38bbd827
reinahandal <handalreina@gmail.com>
2022-08-31 20:04:44 +0300

    attempted solution for temperature conversion challenge

--- Javascript/dojo-weather/script.js
+++ Javascript/dojo-weather/script.js
@@ -5,7 +5,11 @@ var cookieAlert = document.querySelector(".cookie-alert");
 function acceptCookies() {
     cookieAlert.remove();
 }
-var celsius=
+var celsiusTemp = document.querySelectorAll(".temp span");
+var tempOption = document.querySelector("select option");
+var option = document.getElementById("temp-unit").value;
 function changeTempUnit() {
-
+    if (option == '2') {
+        celsiusTemp.innerText = (celsiusTemp.innerText * 9/5) + 32;
+    }
 }
