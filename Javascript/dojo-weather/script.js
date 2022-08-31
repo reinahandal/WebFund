089b06dba53cc56a417919f58f1122224fd53b31
reinahandal <handalreina@gmail.com>
2022-08-31 21:19:39 +0300

    solution done using querySelectorAll and a loop that iterates through the array generated by it to change each span element and convert it to fahrenheit

--- Javascript/dojo-weather/script.js
+++ Javascript/dojo-weather/script.js
@@ -5,11 +5,14 @@ var cookieAlert = document.querySelector(".cookie-alert");
 function acceptCookies() {
     cookieAlert.remove();
 }
-var celsiusTemp = document.querySelectorAll(".temp span");
-var tempOption = document.querySelector("select option");
-var option = document.getElementById("temp-unit").value;
+var tempSpan = document.querySelectorAll(".temp span");
+
 function changeTempUnit() {
-    if (option == '2') {
-        celsiusTemp.innerText = (celsiusTemp.innerText * 9/5) + 32;
+    var option = document.getElementById('temp-unit').value;
+    if (option === '2') {
+        for (var i=0; i<tempSpan.length; i++) {
+            tempSpan[i].innerText = (tempSpan[i].innerText * 9/5) + 32;
+            console.log(tempSpan[i]);
+        }
     }
 }
