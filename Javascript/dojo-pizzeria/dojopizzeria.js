c34b4e92ccc9975e0a636e0a323826ba7bf3857b
reinahandal <handalreina@gmail.com>
2022-08-31 17:13:58 +0300

    fixing mistake with arrays

--- Javascript/dojo-pizzeria/dojopizzeria.js
+++ Javascript/dojo-pizzeria/dojopizzeria.js
@@ -1,19 +1,19 @@
-function pizzaOven(crustType, sauceType, cheeses, toppings) {
-    var pizza = {};
-    pizza.crustType = crustType;
-    pizza.sauceType = sauceType;
-    pizza.cheeses = cheeses;
-    pizza.toppings = toppings;
+function pizzaOven(one, two, three, four) {
+    var pizza = { crustType: one, sauceType: two, cheeses: three, toppings: four };
+    // pizza.crustType = one;
+    // pizza.sauceType = two;
+    // pizza.cheeses = three;
+    // pizza.toppings = four;
     return pizza;
 }
-var pizza1 = pizzaOven("deepdish", "traditional", "mozzarella", "[pepperoni, sausage]");
+var pizza1 = pizzaOven("deepdish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
 console.log(pizza1);
 
-var pizza2 = pizzaOven("hand tossed", "marinara", "[mozzarella, feta]", "[mushrooms, olives, onions]");
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
 console.log(pizza2);
 
-var pizza3 = pizzaOven("stuffed crust", "basil tomato", "buffalo mozzarella", "[mushrooms, black olives, red peppers");
+var pizza3 = pizzaOven("stuffed crust", "basil tomato", "buffalo mozzarella", ["mushrooms", "black olives", "red peppers"]);
 console.log(pizza3);
 
-var pizza4 = pizzaOven("thin crust", "traditional", "[mozzarella, gorgonzola, parmisan, goat cheese]", "[cherry tomatoes, basil");
+var pizza4 = pizzaOven("thin crust", "traditional", ["mozzarella", "gorgonzola", "parmisan", "goat cheese"], ["cherry tomatoes", "basil"]);
 console.log(pizza4);
