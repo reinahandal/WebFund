b60eb62d5a42cfc42aa175b44b8315569d72e58c
reinahandal <handalreina@gmail.com>
2022-08-31 16:37:21 +0300

    add cookie alert, add two functionalities using javascript

---
+++ Javascript/dojo-weather/script.js
@@ -0,0 +1,11 @@
+function cityAlert() {
+    alert("Loading weather report...");
+}
+var cookieAlert = document.querySelector(".cookie-alert");
+function acceptCookies() {
+    cookieAlert.remove();
+}
+var celsius=
+function changeTempUnit() {
+
+}
