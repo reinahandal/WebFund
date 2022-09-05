ab8bf0e8f6d7d6031d30a88f333fce31d9078899
reinahandal <handalreina@gmail.com>
2022-09-05 20:00:55 +0300

    different solution using only two functions to accept or decline requests, shorter code

--- Javascript/making-connections/script.js
+++ Javascript/making-connections/script.js
@@ -1,30 +1,42 @@
+// var friendRequest = document.querySelector("#friendrequest-1");
+// var friendRequest2 = document.querySelector("#friendrequest-2");
+var requestsNum = document.querySelector("#requests");
+var friendsNum = document.querySelector("#connections");
 var userName = document.querySelector(".card-body h1");
+
+
 function changeName() {
     userName.innerText = "Reina Handal";
 }
 
-var friendRequest = document.querySelector("#friendrequest-1");
-var requestsNum = document.querySelector("#requests");
-var friendRequest2 = document.querySelector("#friendrequest-2");
-var friendsNum = document.querySelector("#connections");
+// function removeRequest() {
+//     friendRequest.remove();
+//     var requestsNew = requestsNum.innerText;
+//     requestsNew--;
+//     requestsNum.innerText = requestsNew;
+// }
 
-function removeRequest() {
-    friendRequest.remove();
-    var requestsNew = requestsNum.innerText;
-    requestsNew--;
-    requestsNum.innerText = requestsNew;
+// function removeRequest2() {
+//     friendRequest2.remove();
+//     var requestsNew = requestsNum.innerText;
+//     requestsNew--;
+//     requestsNum.innerText = requestsNew;
+// }
 
+// function addFriend() {
+//     var newFriends = friendsNum.innerText;
+//     newFriends++;
+//     friendsNum.innerText = newFriends;
+// }
+
+function acceptRequest(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestsNum.innerText--;
+    friendsNum.innerText++;
 }
-
-function removeRequest2() {
-    friendRequest2.remove();
-    var requestsNew = requestsNum.innerText;
-    requestsNew--;
-    requestsNum.innerText = requestsNew;
-}
-
-function addFriend() {
-    var newFriends = friendsNum.innerText;
-    newFriends++;
-    friendsNum.innerText = newFriends;
+function declineRequest(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestsNum.innerText--;
 }
