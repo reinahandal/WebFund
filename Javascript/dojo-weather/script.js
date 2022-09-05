029fc7bd6e2e299f3b2c41ee0c01cd118c8043b9
reinahandal <handalreina@gmail.com>
2022-09-05 20:15:41 +0300

    switching locations of if statements, 1 then 2

--- Javascript/dojo-weather/script.js
+++ Javascript/dojo-weather/script.js
@@ -9,15 +9,15 @@ var tempSpan = document.querySelectorAll(".temp span");
 
 function changeTempUnit() {
     var option = document.getElementById('temp-unit').value;
-    if (option === '2') {
+    if (option === '1') {
+        for (var x=0; x<tempSpan.length; x++) {
+        tempSpan[x].innerText = Math.round(5/9 * (tempSpan[x].innerText - 32));
+    }
+}
+    else if (option === '2') {
         for (var i=0; i<tempSpan.length; i++) {
             tempSpan[i].innerText = (tempSpan[i].innerText * 9/5) + 32;
             console.log(tempSpan[i]);
         }
     }
-    else if (option === '1') {
-        for (var x=0; x<tempSpan.length; x++) {
-        tempSpan[x].innerText = Math.round(5/9 * (tempSpan[x].innerText - 32));
-    }
-}
 }
